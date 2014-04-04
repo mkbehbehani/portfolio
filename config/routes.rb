86d6b8700830d767a7eb6cf3ae0100173d66f3c5
Mashallah Behbehani <mkbehbehani@gmail.com>
2014-04-04 00:27:41 -0500

    Sets up posts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Portfolio::Application.routes.draw do
+  resources :posts
+
   resources :projects
 
   root :to => "home#index"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140404052536) do
+ActiveRecord::Schema.define(version: 20140404052715) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: true do |t|
     t.string   "name"
